0ce259b991575ec26e2b19658dd7113f63c5b7d4
IshaAmbadi <87016718+IshaAmbadi@users.noreply.github.com>
2023-11-30 18:52:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-noseX = 0;
-noseY = 0;
+rightWristY = 0;
+rightWristX = 0;
+rightWristScore = 0;
 
 function preload() {
+    ball_touch_paddle = loadSound("");
 }
 
 function setup() {
-	canvas = createCanvas(1240,336);
+	canvas = createCanvas(700,600);
 	canvas.parent('canvas');
 
-	instializeInSetup(mario);
-
 	video = createCapture(VIDEO);
-	video.size(800, 400);
+	video.size(700, 600);
+    video.hide();
 
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
@@ -19,4 +20,27 @@ function setup() {
 
 function modelLoaded() {
 	console.log('Your model has loaded!');
+}
+
+function gotPoses(results){
+    if(results.length > 0) {
+        console.log(results);
+
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristScore = results[0].pose.rightWrist.score;
+    }
+}
+
+function startGame() {
+    game_status = "start";
+    document.getElementById("status").innerHTML = "Game Is Loaded";
+}
+
+function draw() {
+    if (rightWristScore > 0.2) {
+        fill('#FF0000');
+        stroke('#FF0000');
+        circle(rightWristX, rightWristY, 20);
+    }
 }
